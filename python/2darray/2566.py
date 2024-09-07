3780ab9ca864d74e2eb0162ccb30cad20652391d
Henry Shin <bg22915@bristol.ac.uk>
2024-09-07 09:44:37 +0100

    Create 2566.py

---
+++ python/2darray/2566.py
@@ -0,0 +1,17 @@
+# table = [list(map(int, input().spilt())) for _ in range(9)]
+table = []
+for _ in range(9):
+    row = list(map(int, input().split()))
+    table.append(row)
+
+max_num = 0
+max_row, max_col = 0, 0
+for row in range(9):
+    for col in range(9):
+        if max_num <= table[row][col]:
+            max_row = row + 1
+            max_col = col + 1
+            max_num = table[row][col]
+
+print(max_num)
+print(max_row, max_col)
