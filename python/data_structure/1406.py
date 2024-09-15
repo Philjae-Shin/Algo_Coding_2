6eccf2efbb269d9690f7b16eca76ac2b61d84830
Henry Shin <bg22915@bristol.ac.uk>
2024-09-15 23:02:34 +0100

    Improve Time Complexity

--- python/data_structure/1406.py
+++ python/data_structure/1406.py
@@ -1,19 +1,48 @@
-text = list(input())
-cursor = len(text)
+# Time Complexity = O(n)
 
-for _ in range(int(input())):
-    command = list(input().split())
-    if command[0] == 'P':
-        text.insert(cursor, command[1])
-        cursor += 1
-    elif command[0] == 'L':
-        if cursor < 0:
-            cursor -= 1
+# text = list(input())
+# cursor = len(text)
+#
+# for _ in range(int(input())):
+#     command = list(input().split())
+#     if command[0] == 'P':
+#         text.insert(cursor, command[1])
+#         cursor += 1
+#     elif command[0] == 'L':
+#         if cursor > 0:
+#             cursor -= 1
+#     elif command[0] == 'D':
+#         if cursor < len(text):
+#             cursor += 1
+#     else:
+#         if cursor > 0:
+#             text.remove(text[cursor-1])
+#
+# print(''.join(text))
+
+# Time Complexity = O(1)
+
+import sys
+
+st1 = list(sys.stdin.readline().rstrip())
+st2 = []
+
+for _ in range(int(sys.stdin.readline())):
+    command = list(sys.stdin.readline().split())
+    if command[0] == 'L':
+        if st1:
+            st2.append(st1.pop())
+
     elif command[0] == 'D':
-        if cursor < len(text):
-            cursor += 1
-    else:
-        if cursor > 0:
-            text.remove(text[cursor-1])
+        if st2:
+            st1.append(st2.pop())
 
-print(''.join(text))
+    elif command[0] == 'B':
+        if st1:
+            st1.pop()
+
+    else:
+        st1.append(command[1])
+
+st1.extend(reversed(st2))
+print(''.join(st1))
