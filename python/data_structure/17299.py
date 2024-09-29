c6abee5771f7d984efd994d32eed783318346806
Henry Shin <bg22915@bristol.ac.uk>
2024-09-29 23:18:05 +0100

    Create 17299.py

---
+++ python/data_structure/17299.py
@@ -0,0 +1,15 @@
+n = int(input())
+seq = list(map(int, input().split()))
+
+count = [0] * 1000001 # save each num appear time
+stack = []
+res = [-1] * n
+for num in seq:
+    count[num] += 1
+
+for i in range(n):
+    while stack and count[seq[stack[-1]]] < count[seq[i]]:
+        res[stack.pop()] = seq[i]
+    stack.append(i)
+
+print(" ".join(map(str, res)))
