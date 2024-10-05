7ddbb6867932b184c4cd9653c8512f29e9970aee
Henry Shin <bg22915@bristol.ac.uk>
2024-10-05 23:58:57 +0100

    Update Palindrome_Number.py

--- python/data_structure/Palindrome_Number.py
+++ python/data_structure/Palindrome_Number.py
@@ -3,6 +3,7 @@
 # false
 
 def isPalindrome(x: int) -> bool:
+    # Negative != Palindrome
     if x < 0:
         return False
 
@@ -11,8 +12,10 @@ def isPalindrome(x: int) -> bool:
 
     reversed_num = 0
 
+    # revered num and compare
     while x > reversed_num:
         reversed_num = reversed_num * 10 + x % 10
         x //= 10
 
+    # check reversed_num == [:x]
     return x == reversed_num or x == reversed_num // 10
